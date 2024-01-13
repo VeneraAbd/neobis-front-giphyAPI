cd3b5b7193cbe573bef0145cb68502fedd3feffe
VeneraAbd <ven.abdyganyeva@gmail.com>
2024-01-13 17:56:51 +0600

    rendered gifs

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-const search_input = document.querySelector('#search_input').value;
-
-const API_KEY = "WfXZgy5VyrhNFs5tGQPEo7kz4dlrwzA5";
-
+const search_word = document.querySelector('#search_input');
+const search_icon = document.querySelector('.icon');
+const gifContainer = document.querySelector('.main');
+// const debounceTime = 300;
 
 async function fetchGiphyAPI(){
-    let response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=${WfXZgy5VyrhNFs5tGQPEo7kz4dlrwzA5}&q=${search_input}");
-    let data = await response.json();
-    console.log(data)
-}
-fetchGiphyAPI();
+
+    try{
+        let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=WfXZgy5VyrhNFs5tGQPEo7kz4dlrwzA5&q=${search_word.value}`);
+        let data = await response.json();
+
+        // clearing the previous Gifs
+        gifContainer.innerHTML = '';
+       
+        // rendering GIFs
+        data.data.forEach(gif => {
+            const card = document.createElement('img');
+            card.src = gif.images.fixed_height.url;
+            card.classList.add("card");
+            
+            gifContainer.appendChild(card);
+            console.log('caarrrd')
+        });
+
+    }catch(error){
+        console.log('Error fetching data:', error.message)
+    }
+};
+
+// const debouncedFetchGIFs = _.debounce(fetchGiphyAPI, debounceTime);
+
+search_word.addEventListener('input', fetchGiphyAPI);
+search_icon.addEventListener('click', fetchGiphyAPI);
